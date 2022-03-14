df1eb9ac37438db3ba0a5eebc8ba9c25e65b6748
Kukonen <ekukonen@gmail.com>
2022-03-14 19:56:51 +0300

    add items

---
+++ src/main/java/net/evgenniy/fabricfirst/items/ModItems.java
@@ -0,0 +1,27 @@
+package net.evgenniy.fabricfirst.items;
+
+import net.evgenniy.fabricfirst.FabricFirst;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModItems {
+    public static final Item MYTHRIL_INGOT = registerItem("mythril_ingot",
+                new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    public static final Item MYTHRIL_NUGGET = registerItem("mythril_nugget",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    public static final Item RAW_MYTHRIL = registerItem("raw_mythril",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(FabricFirst.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        FabricFirst.LOGGER.info("Register Mod Items for " + FabricFirst.MOD_ID);
+    }
+}
